bc3ea93bbbd1187097b785e06ae5790d128914ab
Amy Wong <amy.wong@monster.com>
2020-10-01 15:11:25 +1000

    Add note

--- src/main/scala/org/jinilover/resuability/ActuallySimply.scala
+++ src/main/scala/org/jinilover/resuability/ActuallySimply.scala
@@ -1,8 +1,10 @@
 package org.jinilover.resuability
 
+// should be imported to use the template code `sequence` and Option, Either instances
 import cats.implicits._
 
 object ActuallySimply {
+  // same as TemplateCode.genericFlip(List(Option(1), Option(2), Option(3)))
   List(Option(1), Option(2), Option(3)).sequence
 
   List[Either[String, Int]](Right(1), Right(2), Right(3)).sequence
--- src/main/scala/org/jinilover/resuability/TemplateCode.scala
+++ src/main/scala/org/jinilover/resuability/TemplateCode.scala
@@ -6,4 +6,12 @@ object TemplateCode {
     xs.foldLeft(m.pure(List.empty[Int])) { (accmulator, ma) =>
       m.bind[List[Int], List[Int]](accmulator, accV => m.fmap[Int, List[Int]](ma, v => accV ++ List(v)))
     }
+
+  // same as `genericFlip` if you want to make the signature more concise
+  // !!note that if you want to call the `M` directly, you should include
+  // `addCompilerPlugin("org.augustjune" %% "context-applied" % "0.1.2")` in `build.sbt`
+  def simplifyGenericFlip[M[_]: Constraint](xs: List[M[Int]]): M[List[Int]] =
+    xs.foldLeft(M.pure(List.empty[Int])) { (accmulator, ma) =>
+      M.bind[List[Int], List[Int]](accmulator, accV => M.fmap[Int, List[Int]](ma, v => accV ++ List(v)))
+    }
 }
